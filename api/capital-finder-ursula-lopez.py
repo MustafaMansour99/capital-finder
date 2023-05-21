a6891c7468ff9973f8eb4ce93f16d7bf157c3542
abostaf <23037172@student.ltuc.com>
2023-05-21 13:33:11 +0000

    first push

---
+++ api/capital-finder-ursula-lopez.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ #handle a request for me 
+ #methode to handle HTTP Get request
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
